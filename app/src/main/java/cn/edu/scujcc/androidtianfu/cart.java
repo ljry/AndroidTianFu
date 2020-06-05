d5a15b7f8496fb6094258a535f05d12032d41611
ZYF <63950267+ZYFnp@users.noreply.github.com>
2020-06-05 13:04:42 +0800

    弱弱代码(cart.java)报错 消错，记得到时候自己改了

--- app/src/main/java/cn/edu/scujcc/androidtianfu/cart.java
+++ app/src/main/java/cn/edu/scujcc/androidtianfu/cart.java
@@ -9,6 +9,6 @@ public class cart extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_choose_dialog);
     }
 }
